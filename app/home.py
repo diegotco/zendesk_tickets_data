cf0cfad39be418d91c5ceba62f6eba2a279a0c4c
MrLouzao <luismiguel.louzao.gonzalez@gmail.com>
2023-02-24 22:32:13 +0900

    Create client for zendesk

--- app/home.py
+++ app/home.py
@@ -1,9 +1,29 @@
-print("<<Program started>>")
-print("Please be patient. It's performing its tasks.")
-print("You'll see a message when it's done.")
-print("////////////////////////////////////////////////////////////")
+import logging
+import sys
 
-import zd_ticket_messages, zd_split_messages, words, send_email
+import config
+import zd_ticket_messages
 
-print("////////////////////////////////////////////////////////////")
-print("<<Program finished>>")
+## Configure logging variables
+logformat = "%(asctime)s %(levelname)s:%(name)s - %(message)s"
+logging.basicConfig(level=logging.INFO, stream=sys.stdout,
+                format=logformat, datefmt="%Y-%m-%dT%H:%M:%S")
+
+## Init of the script
+logging.info("""
+    <<Program started>>
+    Please be patient. It's performing its tasks.
+    You'll see a message when it's done.
+    ////////////////////////////////////////////////////////////
+""")
+
+loaded_config = config.load_configuration()
+zd_ticket_messages.load_zendesk_tickets(loaded_config)
+
+# TODO modify logic inside this scripts
+#import zd_ticket_messages, zd_split_messages, words, send_email
+
+logging.info("""
+    ////////////////////////////////////////////////////////////
+    <<Program finished>>
+""")
